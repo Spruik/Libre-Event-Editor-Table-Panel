7468c1cbb60316ac0fa44a13019496330bb73f02
Tom Hollingworth <i8pizza2@gmail.com>
2021-03-23 22:06:05 -0400

    WIP

--- src/LibreEventEditorTablePanel.tsx
+++ src/LibreEventEditorTablePanel.tsx
@@ -1,140 +1,258 @@
-import React, { Component } from 'react';
+import React, { PureComponent  } from 'react';
 
-import { DataFrame, FieldMatcherID, getFrameDisplayName, PanelProps, SelectableValue } from '@grafana/data';
-import { LibreEventEditorTableOptions } from 'types';
-import { css } from 'emotion';
-import { FilterItem } from '@grafana/ui/components/Table/types';
-import { Select, Table, TableSortByFieldState } from '@grafana/ui';
-import { config  } from '@grafana/runtime';
+import { AppEvents, AppEvent, AlertPayload, AlertErrorPayload, DataFrame, PanelData, PanelProps, BusEventBase, PanelEvents, EventBusSrv } from '@grafana/data';
+import { LibreEventEditorTableOptions, MachineEvent, Reason, MACHINE_EVENT_COLUMNS, REASON_COLUMNS } from 'types';
+// import { css } from 'emotion';
+import { HorizontalGroup, Modal, Button } from '@grafana/ui';
+import { getDataSourceSrv, SystemJS, DashboardInfo } from '@grafana/runtime';
 
+const { alertError, alertSuccess } = AppEvents;
 
-interface Props extends PanelProps<LibreEventEditorTableOptions> {}
+interface Props extends PanelProps<LibreEventEditorTableOptions> {
+}
 
-export class LibreEventEditorTablePanel extends Component<Props> {
+interface State {
+  machineEvent: MachineEvent | null,
+  bus: EventBusSrv
+}
+
+export class RefreshEvent extends BusEventBase {
+  static type = 'refresh';
+}
+
+export class LibreEventEditorTablePanel extends PureComponent<PanelProps, State> {
   constructor(props: Props) {
     super(props);
+    this.state = {
+      machineEvent: null,
+      bus: new EventBusSrv()
+    };
   }
 
-  onColumnResize = (fieldDisplayName: string, width: number) => {
-    const { fieldConfig } = this.props;
-    const { overrides } = fieldConfig;
+  transformEvents = (dataFrame: DataFrame | undefined): MachineEvent[] => {
+    if (!dataFrame) {
+      return []
+    }
+    
+    const idField = dataFrame.fields.find(field => field.name === 'id')
+    const startTimeField = dataFrame.fields.find(field => field.name === 'startDateTime')
+    const endTimeField = dataFrame.fields.find(field => field.name === 'endDateTime')
+    const durationField = dataFrame.fields.find(field => field.name === 'duration')
+    const timeTypeField = dataFrame.fields.find(field => field.name === 'reasonCategoryCode')
+    const categoryField = dataFrame.fields.find(field => field.name === 'reasonCode')
+    const reasonField = dataFrame.fields.find(field => field.name === 'reasonText')
+    const commentField = dataFrame.fields.find(field => field.name === 'comments')
 
-    const matcherId = FieldMatcherID.byName;
-    const propId = 'custom.width';
+    let events: MachineEvent[] = [];
 
-    // look for existing override
-    const override = overrides.find((o) => o.matcher.id === matcherId && o.matcher.options === fieldDisplayName);
-
-    if (override) {
-      // look for existing property
-      const property = override.properties.find((prop) => prop.id === propId);
-      if (property) {
-        property.value = width;
-      } else {
-        override.properties.push({ id: propId, value: width });
-      }
-    } else {
-      overrides.push({
-        matcher: { id: matcherId, options: fieldDisplayName },
-        properties: [{ id: propId, value: width }],
-      });
+    for (let i = 0; i < dataFrame.length; i++) {
+      events.push({
+        id: idField?.values.get(i),
+        startDateTime: startTimeField?.values.get(i),
+        endDateTime: endTimeField?.values.get(i),
+        duration: durationField?.values.get(i),
+        timeType: timeTypeField?.values.get(i),
+        category: categoryField?.values.get(i),
+        reason: reasonField?.values.get(i),
+        comment: commentField?.values.get(i),
+      })
     }
 
-    this.props.onFieldConfigChange({
-      ...fieldConfig,
-      overrides,
-    });
-  };
-
-  onSortByChange = (sortBy: TableSortByFieldState[]) => {
-    this.props.onOptionsChange({
-      ...this.props.options,
-      sortBy,
-    });
-  };
-
-  onChangeTableSelection = (val: SelectableValue<number>) => {
-    this.props.onOptionsChange({
-      ...this.props.options,
-      frameIndex: val.value || 0,
-    });
-
-    // Force a redraw -- but no need to re-query
-    this.forceUpdate();
-  };
-
-  onCellFilterAdded = (filter: FilterItem) => {
-    console.log(`Do nothing with the ${filter}`)
-  };
-
-  renderTable(frame: DataFrame, width: number, height: number) {
-    const { options } = this.props;
-
-    return (
-      <Table
-        height={height}
-        width={width}
-        data={frame}
-        noHeader={!options.showHeader}
-        resizable={true}
-        initialSortBy={options.sortBy}
-        onSortByChange={this.onSortByChange}
-        onColumnResize={this.onColumnResize}
-        onCellFilterAdded={this.onCellFilterAdded}
-        onRowClick={() => {(row) => this.handleRowClick(row)}}
-      />
-    );
+    return events;
   }
 
-  getCurrentFrameIndex() {
-    const { data, options } = this.props;
-    const count = data.series?.length;
-    return options.frameIndex > 0 && options.frameIndex < count ? options.frameIndex : 0;
-  }  
+  transformReasons = (dataFrame: DataFrame | undefined): Reason[] => {
+    if (!dataFrame) {
+      return []
+    }
+    
+    const idField = dataFrame.fields.find(field => field.name === 'id')
+    const isActiveField = dataFrame.fields.find(field => field.name === 'isActaive')
+    const classField = dataFrame.fields.find(field => field.name === 'class')
+    const labelField = dataFrame.fields.find(field => field.name === 'label')
+    const textField = dataFrame.fields.find(field => field.name === 'text')
+    const standardValueField = dataFrame.fields.find(field => field.name === 'standardValue')
+    const equipmentField = dataFrame.fields.find(field => field.name === 'equipment')
+    const equipmentClassField = dataFrame.fields.find(field => field.name === 'equipmentClass')
+
+    let reasons: Reason[] = [];
+
+    for (let i = 0; i < dataFrame.length; i++) {
+      reasons.push({
+        id: idField?.values.get(i),
+        isActive: isActiveField?.values.get(i),
+        class: classField?.values.get(i),
+        label: labelField?.values.get(i),
+        text: textField?.values.get(i),
+        standardValue: standardValueField?.values.get(i),
+        equipment: equipmentField?.values.get(i),
+        equipmentClass: equipmentClassField?.values.get(i),
+      })
+    }
+
+    return reasons;
+  }
+
+  transform = (data: PanelData): {events: MachineEvent[], reasons: Reason[]} => {
+    const eventSerie: DataFrame | undefined = data.series.find((serie) => {
+      for (let i = 0; i < MACHINE_EVENT_COLUMNS.length; i++) {
+        const index = serie.fields.findIndex((field) => {return field.name === MACHINE_EVENT_COLUMNS[i]});
+        if (index < 0) {
+          return false
+        }
+      }
+      return true
+    })
+
+    const reasonSerie: DataFrame | undefined = data.series.find((serie) => {
+      for (let i = 0; i < MACHINE_EVENT_COLUMNS.length; i++) {
+        const index = serie.fields.findIndex((field) => {return field.name === REASON_COLUMNS[i]});
+        if (index < 0) {
+          return false
+        }
+      }
+      return true
+    })
+
+    return {
+      events: this.transformEvents(eventSerie),
+      reasons: this.transformReasons(reasonSerie)
+    }
+  }
+
+  onRowClick = (e: any, row: MachineEvent) => {
+    const { reasons } = this.transform(this.props.data);
+
+    if (reasons && reasons.length > 0) {
+      this.setState({
+        machineEvent: row
+      })
+    }
+  }
+
+  setReason = (event: MachineEvent, reason: Reason) => {
+    const eventsRequest = this.props.data.request?.targets.find((target) => {
+      return target.refId === this.props.options.eventMetric;
+    })
+
+    if (eventsRequest) {
+      getDataSourceSrv().get(eventsRequest.datasource)
+        .then((ds) => {
+          try {
+            //@ts-ignore
+            ds.request(`
+            mutation
+              {
+                updateEventLog(input:{filter:{id:["${event.id}"]}, set: {comments:"Updated via Grafana", reasonText: "${reason.text}"}}){
+                  eventLog{
+                    id
+                    comments
+                    equipment{
+                      name
+                    }
+                    reasonText
+                    reasonCategoryCode
+                    reasonValue
+                  }
+                  numUids
+                }
+              }
+            `)
+              .then((payload: Response) => {
+                if (payload?.status == 200) {
+                  this.alert(alertSuccess, `Event Updated`)
+                  
+                  this.props.eventBus.publish(new RefreshEvent())
+                  
+                  this.dismissModal();
+                }
+              })
+          } catch (error: any) {
+            this.alert(alertError, `Failed to Update: ${error}`)
+          }
+        })
+        .catch((err: Error) => {
+          this.alert(alertError, `Failed to find Event Metric '${this.props.options.eventMetric}': ${err}`)
+        })
+    } else {
+      this.alert(alertError, `Failed to find Event Metric '${this.props.options.eventMetric}'`)
+    }
+  }
+
+  alert = (type: AppEvent<AlertPayload | AlertErrorPayload>, msg: string ) => {
+    SystemJS.load('app/core/app_events')
+      .then((events: any) => {
+        events.emit(type, [msg]);
+      })
+  }
+
+  test = () => {
+    this.props.eventBus.publish(new RefreshEvent());
+    const { bus } = this.state;
+
+    bus.emit(PanelEvents.refresh);
+  }
+
+  dismissModal = () => {
+    this.setState({machineEvent: null})
+  }
 
   render() {
-    const { data, height, width } = this.props;
-    
-    const count = data.series?.length;
-    const hasFields = data.series[0]?.fields.length;
+    const { width } = this.props;
+    const { machineEvent } = this.state;
 
-    if (!count || !hasFields) {
+    const { events, reasons } = this.transform(this.props.data);
+    
+    const count = events?.length;
+    const reasonCount = reasons?.length;
+
+    if (!count || !reasonCount) {
       return <div>No data</div>;
     }
 
-    if (count > 1) {
-      const inputHeight = config.theme.spacing.formInputHeight;
-      const padding = 8 * 2;
-      const currentIndex = this.getCurrentFrameIndex();
-      const names = data.series.map((frame, index) => {
-        return {
-          label: getFrameDisplayName(frame),
-          value: index,
-        };
-      });
-
-      return (
-        <div className={tableStyles.wrapper}>
-          {this.renderTable(data.series[currentIndex], width, height - inputHeight - padding)}
-          <div className={tableStyles.selectWrapper}>
-            <Select options={names} value={names[currentIndex]} onChange={this.onChangeTableSelection} />
-          </div>
-        </div>
-      );
-    }
-
-    return this.renderTable(data.series[0], width, height - 12);
+    return <>
+      {
+        machineEvent
+          ? <Modal isOpen={!!machineEvent} title={"Reason"} onDismiss={this.dismissModal} onClickBackdrop={this.dismissModal}>
+            <p>Select the Reason for this event.</p>
+            <HorizontalGroup spacing="lg">
+              {reasons.length > 0 &&
+                reasons.map((reason) => {
+                  return (<Button size={"md"} onClick={() => {this.setReason(machineEvent, reason)}}>{reason.text}</Button>)
+                })}
+            </HorizontalGroup>
+          </Modal>
+          : <></>
+      }
+      <table width={width}>
+        <thead>
+          <th>Start</th>
+          <th>End</th>
+          <th>Duration</th>
+          <th>Time Category</th>
+          <th>Reason</th>
+          <th>Comment</th>
+        </thead>
+        <tbody>
+          { events.length > 0 &&
+          events.map((event) => {
+            return (
+              <>
+                <tr onClick={(e) => {return this.onRowClick(e, event)}}>
+                  <td>{event.startDateTime}</td>
+                  <td>{event.endDateTime}</td>
+                  <td>{event.duration}</td>
+                  <td>{event.timeType}</td>
+                  <td>{event.reason}</td>
+                  <td>{event.comment}</td>
+                </tr>
+              </>
+            )
+          })
+          }
+        </tbody>
+      </table>
+      <Button onClick={this.test}>Test</Button>
+    </>
   }
 }
-
-const tableStyles = {
-  wrapper: css`
-    display: flex;
-    flex-direction: column;
-    justify-content: space-between;
-    height: 100%;
-  `,
-  selectWrapper: css`
-    padding: 8px;
-  `,
-};
--- src/types.ts
+++ src/types.ts
@@ -1,17 +1,51 @@
-import { TableSortByFieldState } from '@grafana/ui';
 
 export interface LibreEventEditorTableOptions {
-  frameIndex: number;
-  showHeader: boolean;
-  sortBy?: TableSortByFieldState[];
+  eventMetric: string;
+  reasonMetric: string;
 }
 
-export interface TableSortBy {
-  displayName: string;
-  desc: boolean;
+
+export const MACHINE_EVENT_COLUMNS = [
+  "id", 
+  "startDateTime",
+  "endDateTime",
+  "duration",
+  "reasonCategoryCode",
+  "reasonText",
+  "reasonCode",
+  "comments"
+]
+
+export interface MachineEvent {
+  id: string;
+  startDateTime: number;
+  endDateTime?: number;
+  duration: number;
+  timeType: string;
+
+  category?: string;
+  reason?: string;
+  comment: string;
 }
 
-export interface CustomFieldConfig {
-  width: number;
-  displayMode: string;
+export const REASON_COLUMNS = [
+  "id", 
+  "isActive",
+  "class",
+  "label",
+  "text",
+  "standardValue",
+  "equipment",
+  "equipmentClass"
+]
+
+export interface Reason {
+  id: string;
+  isActive: boolean;
+  class: string;
+  label: string;
+  text: string;
+  standardValue: string;
+  equipment: Object
+  equipmentClass: Object
 }
